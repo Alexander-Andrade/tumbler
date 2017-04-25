99a7bf4fe0578db7a3f7b1ec4f768ceac8c9fafe
Alexander-Andrade <veimar.94@gmail.com>
2017-04-25 12:49:09 +0300

    icons for devs and controls

---
+++ app/assets/javascripts/services/device-specific-info.service.js
@@ -0,0 +1,66 @@
+(function() {
+    'use strict';
+
+    angular.module('services').factory('deviceInfo', ['', function() {
+        var info = {
+            device: {
+                lamp: {
+                    icon: 'ion-ios-lightbulb',
+                    control: {
+                        toggle: {
+                            onn: 'ion-ios-lightbulb-outline',
+                            off: 'ion-ios-lightbulb'
+                        }
+                    }
+                },
+                temperature_sensor: {
+                    icon: 'ion-thermometer'
+                },
+                light_sensor: {
+                    icon: 'ion-android-sunny'
+                },
+                door_sensor: {
+                    icon: 'ion-key',
+                    control: {
+                        toggle: {
+                            onn: 'ion-ios-unlocked',
+                            off: 'ion-ios-locked'
+                        }
+                    }
+                },
+                smoke_detector: {
+                    icon: 'ion-no-smoking'
+                },
+                water_leakage_sensor: {
+                    icon: 'ion-waterdrop'
+                },
+                gas_sensor: {
+                    icon: 'fa fa-bomb'
+                },
+                motion_or_sound_sensor: {
+                    icon: 'ion-ios-volume-high'
+                },
+                unknown_device: {
+                    icon: 'ion-help',
+                    control: {
+                        toggle: {
+                            onn: 'ion-toggle-filled',
+                            off: 'ion-toggle'
+                        }
+                    }
+                }
+            }
+        };
+
+        info.infoByLabel = function (label) {
+            var dev = info.device[label];
+            if(_.isUndefined(dev)){
+                dev = info.device.unknown_device;
+            }
+            return dev;
+        };
+
+        return info;
+    }]);
+
+}());
