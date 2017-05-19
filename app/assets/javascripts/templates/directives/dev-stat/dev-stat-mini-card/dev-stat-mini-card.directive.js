9ace1b13224ff563be7bed0b2572dab6527bf9d7
Alexander-Andrade <veimar.94@gmail.com>
2017-05-19 12:05:05 +0300

    show labels lowercase

--- app/assets/javascripts/templates/directives/dev-stat/dev-stat-mini-card/dev-stat-mini-card.directive.js
+++ app/assets/javascripts/templates/directives/dev-stat/dev-stat-mini-card/dev-stat-mini-card.directive.js
@@ -3,13 +3,21 @@
 
     angular.module('directives').
     directive('devStatMiniCard', function () {
+
+        var ctrl = ['$scope', function ($scope) {
+            $scope.formatLabel = function () {
+              return _.lowerCase($scope.label);
+            };
+        }];
+
         var directive =  {
             templateUrl: 'directives/dev-stat/dev-stat-mini-card/dev-stat-mini-card.html',
             scope: {
                 label: '=',
                 icon: '=',
                 number: '='
-            }
+            },
+            controller: ctrl
         };
 
         return directive;
